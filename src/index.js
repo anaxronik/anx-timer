b3cf44cbf12c2cc3e4cfa2b9a8baff51f6eca5a6
Sergey Komarov <anaxronik@gmail.com>
2020-12-21 17:46:23 +0500

    add react to web pack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import './index.scss'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App/App.js'
 
-console.log('script start')
+ReactDOM.render(<App />, document.getElementById('root'))
